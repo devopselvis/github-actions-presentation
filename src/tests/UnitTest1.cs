2b1f4776aa3750381b85a2add27127d63b7a17cb
Mickey Gousset <mickeygousset@github.com>
2022-06-02 00:22:55 +0000

    #1 - updated unit tests

--- src/tests/UnitTest1.cs
+++ src/tests/UnitTest1.cs
@@ -5,21 +5,65 @@ namespace tests
 {
     public class UnitTest1
     {
+        
         [Fact]
-        public void Test1()
+        public void PassesTestOne()
         {
-
+            Assert.Equal(42, (21 + 21));
         }
 
         [Fact]
-        public void PassingTest()
+        public void PassesTestTwo()
         {
-            Assert.Equal(4, Add(2, 2));
+            Assert.Equal(99, (33 * 3));
         }
 
-        int Add(int x, int y)
+        [Fact]
+        public void PassesTestThree()
         {
-            return x + y;
+            Assert.Equal("hello", "he" + "llo");
+        }
+
+        [Fact]
+        public void PassesTestFour()
+        {
+            Assert.Equal("world", "world" + "");
+        }
+
+        [Fact]
+        public void FailsTestFive()
+        {
+            Assert.Equal(42, (42 + 1));
+        }
+
+        [Fact]
+        public void FailsTestSix()
+        {
+            Assert.Equal(99, (99 - 1));
+        }
+
+        [Fact]
+        public void FailsTestSeven()
+        {
+            Assert.Equal("hello", "wo" + "rld");
+        }
+
+        [Fact]
+        public void FailsTestEight()
+        {
+            Assert.Equal("world", "hello" + "");
+        }
+
+        [Fact(Skip="skips test nine")]
+        public void SkipsTestNine()
+        {
+            Assert.Equal(42, 99);
+        }
+
+        [Fact(Skip="skips test ten")]
+        public void SkipsTestTen()
+        {
+            Assert.Equal(72, 72);
         }
     }
 }
