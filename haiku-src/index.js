cdf83eda2b02a982f04db91577f5a64df6b8ee5f
Mickey Gousset <mickeygousset@github.com>
2023-08-03 19:37:33 -0500

    Added workflow for PR

--- haiku-src/index.js
+++ haiku-src/index.js
@@ -25,4 +25,9 @@ app.get('/', (req, res) => {
   res.render('index', {haikus: haikus});
 });
 
+app.get('/haikus', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send({haikus: haikus});
+});
+
 module.exports = app;
